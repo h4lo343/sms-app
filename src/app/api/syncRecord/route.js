e6e0533734ccfa8e9f782c937cbc8654e753013d
h4lo343 <92515186+h4lo343@users.noreply.github.com>
2024-07-01 17:47:32 +1000

    fix/fix-the-sync-report-pagination

--- src/app/api/syncRecord/route.js
+++ src/app/api/syncRecord/route.js
@@ -1,10 +1,10 @@
-import getReport from "@/app/utils/getReport";
+import getSyncReport from "@/app/utils/getSyncReport";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const taskUuid = request.nextUrl.searchParams.get("taskUuid");
   if (!taskUuid)
     return NextResponse.json({ msg: "task uuid missed" }, { status: 400 });
-  const report = await getReport(taskUuid);
+  const report = await getSyncReport(taskUuid);
   return NextResponse.json({ data: report }, { status: 200 });
 }
--- src/app/utils/getSyncReport.js
+++ src/app/utils/getSyncReport.js
@@ -1,24 +1,27 @@
 import { link } from "fs";
 import queryString from "querystring";
 import getLinkCount from "./getLinkCount";
-
+let nextLink;
 const vonageKey = process.env.VONAGE_API_KEY;
 const vonageSecret = process.env.VONAGE_API_SECRET;
+// const baseUrl = `https://api.nexmo.com/v2/reports/records`;
 const baseUrl = `https://api.nexmo.com/v2/reports/records`;
 const authStr = Buffer.from(vonageKey + ":" + vonageSecret).toString("base64");
-
+const records = [];
 export default async function getReport(clientRef) {
   const linkId = clientRef.split("-")[1];
   const linkInfo = await getLinkCount(linkId);
   const params = queryString.stringify({
     account_id: vonageKey,
-    // client_ref: clientRef,
-    id: "e67bc8f2-16a7-4d28-b139-b36cf35c3e3d",
-    // date_start: "2024-06-14T10:56:27.000Z",
+    client_ref: clientRef,
+    // id: "e67bc8f2-16a7-4d28-b139-b36cf35c3e3d",
+    date_start: "2024-06-14T10:56:27.000Z",
     product: "SMS",
+
     direction: "outbound",
   });
   const requestUrl = baseUrl + "?" + params;
+
   const result = {};
   const non_delivered_numbers = [];
   result.taskUuid = clientRef;
@@ -30,10 +33,13 @@ export default async function getReport(clientRef) {
     },
     cache: "no-store",
   }).then((res) => res.json());
-  console.log("----------vonage_record---------------");
-  console.log(response);
-  console.log("----------vonage_record---------------");
-  for (let report of response.records) {
+  nextLink = response._links?.next?.href;
+  records.push(...response.records);
+  while (nextLink) {
+    await continueRequest(nextLink);
+  }
+  console.log(`----------------${records.length} records got------------`);
+  for (let report of records) {
     const status = report.status;
     if (!result[status]) {
       result[status] = 1;
@@ -46,5 +52,19 @@ export default async function getReport(clientRef) {
   }
   result.non_delivered_numbers = non_delivered_numbers;
   result.linkInfo = linkInfo;
+
   return result;
+}
+
+async function continueRequest(link) {
+  const response = await fetch(link, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${authStr}`,
+    },
+    cache: "no-store",
+  }).then((res) => res.json());
+  nextLink = response._links?.next?.href;
+  records.push(...response.records);
 }
